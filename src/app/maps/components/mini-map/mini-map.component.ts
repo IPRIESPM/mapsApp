8b1a4ab42933c2ba102ee5e8e288b92ca3bc7a10
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-16 12:21:03 +0200

    feat: :tada: start proyect

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-mini-map',
+  templateUrl: './mini-map.component.html',
+  styleUrl: './mini-map.component.css',
+})
+export class MiniMapComponent {}
