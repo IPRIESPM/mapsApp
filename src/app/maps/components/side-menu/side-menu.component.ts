235a75e2da9fd80139492149320de4237c18d15b
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-16 12:36:33 +0200

    feat: :sparkles: add a side-menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  route: string;
+  name: string;
+}
+
 @Component({
   selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'Fullscreen' },
+    { route: '/maps/zoom-range', name: 'zoom range' },
+    { route: '/maps/markers', name: 'markers' },
+    { route: '/maps/properties', name: 'Houses' },
+  ];
+}
