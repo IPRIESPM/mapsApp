6b1b49caf980c3543f734631e8bacc6bd495572b
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-17 11:31:15 +0200

    feat: :sparkles: add stanalone components

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css',
+})
+export class AlonePageComponent {}
