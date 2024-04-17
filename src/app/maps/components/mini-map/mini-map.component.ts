1ee20e9e353302116cd51f2f2e0bc73e07f64325
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-17 09:41:52 +0200

    feat: :sparkles: add properties-page and mini-map component

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css',
 })
-export class MiniMapComponent {}
+export class MiniMapComponent {
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement)
+      throw new Error('mapContainer is required');
+    if (!this.lngLat) throw new Error('lngLat is required');
+
+    const map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
+}
